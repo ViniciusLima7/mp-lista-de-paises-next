733728af04108728bcd517102c96a7861a31016a
Vinicius Lima <vinicius.lima_iasd@hotmail.com>
2024-06-23 22:19:44 -0300

    feat: add header in Application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
@@ -16,7 +17,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunitoSans.className}>{children}</body>
+      <body className={nunitoSans.className}>
+        <main className="bg-gray-100 min-h-screen">
+          <nav className="w-full bg-white h-16 flex items-center justify-center">
+            <section className="container flex items-center gap-3">
+              <Image
+                src="/logo.svg"
+                alt="logo application - emoji globe"
+                width={48}
+                height={48}
+              />
+              <h1 className="font-bold text-2xl">List Country</h1>
+            </section>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
