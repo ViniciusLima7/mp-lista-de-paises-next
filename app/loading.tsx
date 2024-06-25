283fa69a8fa4acfb8f69316d9ef99fb092c3d742
Vinicius Lima <vinicius.lima_iasd@hotmail.com>
2024-06-24 22:28:37 -0300

    feat: add loading

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <section className="grid grid-cols-5 container">
+      <h1 className="text-5xl text-center font-bold text-gray-800 my-16">
+        Carregando...
+      </h1>
+    </section>
+  );
+}
